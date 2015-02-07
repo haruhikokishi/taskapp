50eec5a3119b3916161cdf7bde8cdcd545ddab55
Haruhiko KISHI <haruhiko.kishi@gmail.com>
2015-02-07 15:36:03 +0900

    generate model Task generate controller Tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150207035804) do
+ActiveRecord::Schema.define(version: 20150207062905) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -19,4 +19,15 @@ ActiveRecord::Schema.define(version: 20150207035804) do
     t.datetime "updated_at",             null: false
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.boolean  "done",       limit: 1
+    t.integer  "project_id", limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "tasks", ["project_id"], name: "index_tasks_on_project_id", using: :btree
+
+  add_foreign_key "tasks", "projects"
 end
